a99043cd6087f323fa0ad413fa773a167f41a17b
chiqui3d <nerjacarloscampos@gmail.com>
2020-08-16 14:16:11 +0100

    first commit

---
+++ content-manager/admin/src/components/WysiwygWithErrors/Tinymce.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Editor as Tinymce } from '@tinymce/tinymce-react';
+import styled from 'styled-components';
+import { auth } from 'strapi-helper-plugin';
+
+const Wrapper = styled.div`
+  .ck-editor__main {
+    min-height: 200px;
+    > div {
+      min-height: 200px;
+    }
+  }
+`;
+
+const Editor = ({ onChange, name, value }) => {
+
+  const jwtToken = auth.getToken();
+
+  return (
+    <>
+      <Wrapper>
+        <Tinymce
+          initialValue=""
+          tinymceScriptSrc='/tinymce/js/tinymce/tinymce.min.js'
+          value={value}
+          init={{
+            height: 500,
+            menubar: false,
+            // eslint-disable-next-line @typescript-eslint/camelcase
+            toolbar_mode: 'wrap',
+            plugins: [
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste code help wordcount',
+              'media codesample fullscreen',
+              'hr visualchars imagetools emoticons'
+            ],
+            toolbar:
+            // eslint-disable-next-line no-multi-str
+              'undo redo | formatselect forecolor backcolor | \
+              bold italic underline strikethrough removeformat | \
+              alignleft aligncenter alignright alignjustify | \
+              outdent indent | numlist bullist | \
+              table link anchor | image media codesample charmap emoticons | \
+              fullscreen code'
+          }}
+          onEditorChange={(content, editor) => {
+            onChange({ target: { name, value: content } });
+          }}
+        />
+
+      </Wrapper>
+    </>
+  );
+};
+
+Editor.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+};
+
+export default Editor;
---
+++ content-manager/admin/src/components/WysiwygWithErrors/index.js
@@ -0,0 +1,102 @@
+import React, {useState} from 'react';
+import PropTypes from 'prop-types';
+import { isEmpty } from 'lodash';
+import { Button } from '@buffetjs/core';
+import { Label, InputDescription, InputErrors } from 'strapi-helper-plugin';
+import Editor from './Tinymce';
+import MediaLib from './MediaLib';
+
+const WysiwygWithErrors = ({
+  inputDescription,
+  errors,
+  label,
+  name,
+  noErrorsDescription,
+  onChange,
+  value,
+}) => {
+  const [isOpen, setIsOpen] = useState(false);
+  let spacer = !isEmpty(inputDescription) ? (
+    <div style={{ height: '.4rem' }} />
+  ) : (
+    <div />
+  );
+
+  if (!noErrorsDescription && !isEmpty(errors)) {
+    spacer = <div />;
+  }
+  const handleToggle = () => setIsOpen(prev => !prev);
+  const handleChange = data => {
+    if (data.mime.includes('image')) {
+      const imgTag = `<p><img src="${data.url}" caption="${data.caption}" alt="${data.alternativeText}"></img></p>`;
+      const newValue = value ? `${value}${imgTag}` : imgTag;
+
+      onChange({ target: { name, value: newValue } });
+    }
+
+    // Handle videos and other type of files by adding some code
+  };
+
+  return (
+    <div
+      className="col-12"
+      style={{
+        marginBottom: '1.6rem',
+        fontSize: '1.3rem',
+        fontFamily: 'Lato',
+      }}
+    >
+      <Label htmlFor={name} message={label} style={{ marginBottom: 10 }} />
+      <div>
+        <Button style={{lineHeight: '1', marginBottom: '16px'}} color="primary" onClick={handleToggle}>
+          Media Library
+        </Button>
+      </div>
+      <Editor name={name} onChange={onChange} value={value} />
+      <InputDescription
+        message={inputDescription}
+        style={!isEmpty(inputDescription) ? { marginTop: '1.4rem' } : {}}
+      />
+      <InputErrors
+        errors={(!noErrorsDescription && errors) || []}
+        name={name}
+      />
+      {spacer}
+      <MediaLib onToggle={handleToggle} isOpen={isOpen} onChange={handleChange} />
+    </div>
+  );
+};
+
+WysiwygWithErrors.defaultProps = {
+  errors: [],
+  inputDescription: null,
+  label: '',
+  noErrorsDescription: false,
+  value: '',
+};
+
+WysiwygWithErrors.propTypes = {
+  errors: PropTypes.array,
+  inputDescription: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+    PropTypes.shape({
+      id: PropTypes.string,
+      params: PropTypes.object,
+    }),
+  ]),
+  label: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.func,
+    PropTypes.shape({
+      id: PropTypes.string,
+      params: PropTypes.object,
+    }),
+  ]),
+  name: PropTypes.string.isRequired,
+  noErrorsDescription: PropTypes.bool,
+  onChange: PropTypes.func.isRequired,
+  value: PropTypes.string,
+};
+
+export default WysiwygWithErrors;
